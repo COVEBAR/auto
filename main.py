a9f05c5161833efddd3964c915ddde5ff8e80ec5
COVEBAR <ivashkam113@gmail.com>
2026-04-23 20:59:12 +0700

    just added menu

---
+++ main.py
@@ -0,0 +1,17 @@
+while True:
+    show_menu()
+    choice = input("Выберите действие (1-5): ").strip()
+
+    if choice == "1":
+        break
+    elif choice == "2":
+        break
+    elif choice == "3":
+        break
+    elif choice == "4":
+        break
+    elif choice == "5":
+        print("\nПрограмма завершена.")
+        break
+    else:
+        print("Ошибка: неверный выбор. Пожалуйста, выберите от 1 до 5.")
